6957ffa51564dd02601031b87c320f5f78caa33d
Wilson Bienvenido Otaño Mateo <102930523+Starmustang@users.noreply.github.com>
2025-05-18 21:04:34 -0400

    mejora menor

--- src/app/(DashboardLayout)/layout/vertical/header/Header.tsx
+++ src/app/(DashboardLayout)/layout/vertical/header/Header.tsx
@@ -1,18 +1,12 @@
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 import Toolbar from '@mui/material/Toolbar';
-import useMediaQuery from '@mui/material/useMediaQuery';
 import { styled } from '@mui/material/styles';
 
 import { CustomizerContext } from '@/app/context/customizerContext';
 import { Icon } from "@iconify/react";
-import Notifications from "./Notification";
 import Profile from "./Profile";
-import Cart from "./Cart";
-import Search from "./Search";
-import Language from "./Language";
 
 import { shadows } from "@/utils/theme/Shadows";
 import React, { useContext } from "react";
@@ -20,23 +14,18 @@ import IconButton from '@mui/material/IconButton';
 import config from '@/app/context/config';
 
 const Header = () => {
-  const lgUp = useMediaQuery((theme: any) => theme.breakpoints.up("lg"));
-  const smUp = useMediaQuery((theme: any) => theme.breakpoints.up("sm"));
-
-
+  
   const TopbarHeight = config.topbarHeight;
 
   // drawer
-  const { activeMode, setActiveMode, setIsCollapse, isCollapse, isMobileSidebar, setIsMobileSidebar } = useContext(CustomizerContext);
+  const { activeMode, setActiveMode } = useContext(CustomizerContext);
 
   const AppBarStyled = styled(AppBar)(({ theme }) => ({
     boxShadow: shadows[9],
     background: theme.palette.background.paper,
     justifyContent: "center",
     backdropFilter: "blur(4px)",
-    // [theme.breakpoints.up("lg")]: {
     minHeight: TopbarHeight,
-    // },
     borderRadius: 13,
   }));
   const ToolbarStyled = styled(Toolbar)(({ theme }) => ({
@@ -48,44 +37,8 @@ const Header = () => {
   return (
     <AppBarStyled position="sticky" color="default">
       <ToolbarStyled>
-        <Stack spacing={1} direction="row" alignItems="center">
-          {/* ------------------------------------------- */}
-          {/* Toggle Button Sidebar */}
-          {/* ------------------------------------------- */}
-          <Button
-            color="inherit"
-            aria-label="menu"
-            size="large"
-            className="btn-rounded-circle-40"
-            onClick={() => {
-              // Toggle sidebar on both mobile and desktop based on screen size
-              if (lgUp) {
-                // For large screens, toggle between full-sidebar and mini-sidebar
-                isCollapse === "full-sidebar" ? setIsCollapse("mini-sidebar") : setIsCollapse("full-sidebar");
-              } else {
-                // For smaller screens, toggle mobile sidebar
-                setIsMobileSidebar(!isMobileSidebar);
-              }
-            }}
-          >
-            <Icon icon="solar:list-bold-duotone" width="24" height="24" />
-          </Button>
-
-          {/* ------------------------------------------- */}
-          {/* Search Dropdown */}
-          {/* ------------------------------------------- */}
-        </Stack>
         <Box flexGrow={1} />
         <Stack spacing={2} direction="row" alignItems="center">
-          {smUp ? <Search /> : ""}
-          <Language />
-          {/* ------------------------------------------- */}
-          {/* Ecommerce Dropdown */}
-          {/* ------------------------------------------- */}
-          <Cart />
-          {/* ------------------------------------------- */}
-          {/* End Ecommerce Dropdown */}
-          {/* ------------------------------------------- */}
           <IconButton size="large" color="inherit">
             {activeMode === 'light' ? (
               <Icon icon="solar:moon-line-duotone" width="21" height="21" onClick={() => setActiveMode("dark")} />
@@ -93,7 +46,7 @@ const Header = () => {
               <Icon icon="solar:sun-2-line-duotone" width="21" height="21" onClick={() => setActiveMode("light")} />
             )}
           </IconButton>
-          <Notifications />
+          
           <Profile />
         </Stack>
       </ToolbarStyled>
--- src/app/(DashboardLayout)/layout/vertical/sidebar/MenuItems.ts
+++ src/app/(DashboardLayout)/layout/vertical/sidebar/MenuItems.ts
@@ -21,99 +21,38 @@ interface MenuitemsType {
 const Menuitems: MenuitemsType[] = [
   {
     navlabel: true,
-    subheader: "Home",
+    subheader: "Pagina principal",
   },
 
   {
     id: uniqueId(),
-    title: "Sample Page",
+    title: "Pagina principal",
     icon: 'screencast-2-line-duotone',
     href: "/",
     bgcolor: "primary",
   },
-  {
-    navlabel: true,
-    subheader: "Other",
-  },
+
   {
     id: uniqueId(),
-    title: "Menu Level",
-    icon: 'double-alt-arrow-down-bold-duotone',
-    href: "/menulevel/",
+    title: "Doctor",
+    icon: 'screencast-2-line-duotone',    
     bgcolor: "primary",
-    children: [
-      {
-        id: uniqueId(),
-        title: "Level 1",
-        href: "/l1",
-      },
-      {
-        id: uniqueId(),
-        title: "Level 1.1",
-        href: "/l1.1",
-        children: [
-          {
-            id: uniqueId(),
-            title: "Level 2",
-            href: "/l2",
-          },
-          {
-            id: uniqueId(),
-            title: "Level 2.1",
-            href: "/l2.1",
-            children: [
-              {
-                id: uniqueId(),
-                title: "Level 3",
-                href: "/l3",
-              },
-              {
-                id: uniqueId(),
-                title: "Level 3.1",
-                href: "/l3.1",
-              },
-            ],
-          },
-        ],
-      },
-    ],
-  },
-  {
-    id: uniqueId(),
-    title: "Disabled",
-    icon: 'forbidden-circle-line-duotone',
-    bgcolor: "secondary",
-    href: "",
-    disabled: true,
-  },
-  {
-    id: uniqueId(),
-    title: "SubCaption",
-    subtitle: "This is the sutitle",
-    icon: 'square-academic-cap-line-duotone',
-    bgcolor: "warning",
-    href: "",
+    href: "/",
   },
 
   {
     id: uniqueId(),
-    title: "Chip",
-    icon: 'archive-check-line-duotone',
-    bgcolor: "error",
-    href: "",
-    chip: "9",
-    chipColor: "error",
+    title: "Paciente",
+    icon: 'screencast-2-line-duotone',    
+    bgcolor: "primary",
+    href: "/",
   },
+
   {
-    id: uniqueId(),
-    title: "Outlined",
-    icon: 'smile-circle-line-duotone',
-    bgcolor: "info",
-    href: "",
-    chip: "outline",
-    variant: "outlined",
-    chipColor: "info",
+    navlabel: true,
+    subheader: "Otros",
   },
+  
   {
     id: uniqueId(),
     title: "External Link",
