6521fbfd4712f11da0c5e330290f0a9c4f758ba8
Wilson Bienvenido Otaño Mateo <102930523+Starmustang@users.noreply.github.com>
2025-09-22 21:51:33 -0400

    CRUD doctoraddress funcional

--- src/app/(DashboardLayout)/address/doctorAddress/components/doctorAddressModal.tsx
+++ src/app/(DashboardLayout)/address/doctorAddress/components/doctorAddressModal.tsx
@@ -3,17 +3,38 @@ import { Dialog, DialogContent, DialogTitle, DialogActions, Button } from "@mui/
 import { useMainStore } from "@/app/store";
 import DoctorAddressForm from "./doctorAddressForm";
 import { useForm, FormProvider } from "react-hook-form";
-import { DoctorAddress } from "@/app/(DashboardLayout)/types/Address/doctorAddress/doctorAddress";
+import { DoctorAddress, DoctorAddressPost, DoctorAddressPut } from "@/app/(DashboardLayout)/types/Address/doctorAddress/doctorAddress";
+import { useEffect } from "react";
 
 const DoctorAddressModal = () => {
-    const { showDoctorAddressModal, handleCloseDoctorAddressModal } = useMainStore();
-    const formMethods = useForm<DoctorAddress>();
+        const { showDoctorAddressModal, handleCloseDoctorAddressModal, getSectorList, createDoctorAddress, updateDoctorAddress, doctorAddressId, getDoctorAddressDetailed, doctorAddressDetailed } = useMainStore();
+        const formMethods = useForm<DoctorAddress>({ 
+        defaultValues: { 
+            doctorHouseNumber: '', 
+            doctorStreet: '', 
+            sectorId: 0 
+        } 
+    });
     const { handleSubmit, reset } = formMethods;
 
     const onSubmit = (data: DoctorAddress) => {
-        console.log(data);
-        handleCloseDoctorAddressModal();
-        reset();
+        if (doctorAddressId) {
+            const doctorAddressToUpdate: DoctorAddressPut = {
+                doctorAddressId: doctorAddressId,
+                doctorHouseNumber: data.doctorHouseNumber,
+                doctorStreet: data.doctorStreet,
+                sectorId: data.sectorId,
+            };
+            updateDoctorAddress(doctorAddressToUpdate);
+        } else {
+            const doctorAddressToCreate: DoctorAddressPost = {
+                doctorHouseNumber: data.doctorHouseNumber,
+                doctorStreet: data.doctorStreet,
+                sectorId: data.sectorId,
+            };
+            createDoctorAddress(doctorAddressToCreate);
+        }
+        handleClose();
     };
 
     const handleClose = () => {
@@ -21,12 +42,30 @@ const DoctorAddressModal = () => {
         reset();
     };
 
+    useEffect(() => {
+        getSectorList();
+    }, [getSectorList]);
+
+    useEffect(() => {
+        if (doctorAddressId) {
+            getDoctorAddressDetailed(doctorAddressId);
+        }
+    }, [doctorAddressId, getDoctorAddressDetailed]);
+
+    useEffect(() => {
+        if (doctorAddressDetailed && doctorAddressId) {
+            formMethods.setValue('doctorHouseNumber', doctorAddressDetailed.doctorHouseNumber);
+            formMethods.setValue('doctorStreet', doctorAddressDetailed.doctorStreet);
+            formMethods.setValue('sectorId', doctorAddressDetailed.sectorId);
+        }
+    }, [doctorAddressDetailed, doctorAddressId, formMethods.setValue]);
+
     return (
         <Dialog
             open={showDoctorAddressModal}
             onClose={handleClose}
         >
-            <DialogTitle>Agregar Dirección de Doctor</DialogTitle>
+            <DialogTitle>{doctorAddressId ? 'Editar' : 'Agregar'} Dirección de Doctor</DialogTitle>
             <DialogContent>
                 <FormProvider {...formMethods}>
                     <DoctorAddressForm />
@@ -34,7 +73,7 @@ const DoctorAddressModal = () => {
             </DialogContent>
             <DialogActions>
                 <Button onClick={handleClose} color="error" variant="contained">Cancelar</Button>
-                <Button onClick={handleSubmit(onSubmit)} color="primary" variant="contained">Guardar</Button>
+                <Button onClick={handleSubmit(onSubmit)} color="primary" variant="contained">{doctorAddressId ? 'Editar' : 'Guardar'}</Button>
             </DialogActions>
         </Dialog>
     );
