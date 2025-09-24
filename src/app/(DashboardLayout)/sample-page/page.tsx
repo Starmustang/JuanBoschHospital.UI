95e25d47003a88265074fdee8f97370eade6a506
Wilson Bienvenido Otaño Mateo <102930523+Starmustang@users.noreply.github.com>
2025-09-23 21:05:37 -0400

    pantalla de inicio

--- src/app/(DashboardLayout)/sample-page/page.tsx
+++ src/app/(DashboardLayout)/sample-page/page.tsx
@@ -5,9 +5,19 @@ import DashboardCard from "@/app/components/shared/DashboardCard";
 
 const SamplePage = () => {
   return (
-    <PageContainer title="Sample Page" description="this is Sample page">
-      <DashboardCard title="Sample Page">
-        <Typography>This is a sample2 page</Typography>
+    <PageContainer title="Página de Bienvenida" description="Esta es la página de bienvenida">
+      <DashboardCard title="¡Bienvenido!">
+        <div>
+          <Typography variant="h5" component="h1" gutterBottom>
+            Bienvenido al Sistema de Gestión del Hospital Juan Bosch
+          </Typography>
+          <Typography variant="body1">
+            Desde este panel, usted podrá gestionar la información de pacientes, médicos, citas y mucho más.
+          </Typography>
+          <Typography variant="body1" sx={{ mt: 2 }}>
+            Utilice el menú de navegación a la izquierda para acceder a las diferentes secciones del sistema.
+          </Typography>
+        </div>
       </DashboardCard>
     </PageContainer>
   );
